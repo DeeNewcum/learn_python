a16246210085b75dbd0dbcb73e2606188817f85f
Dee Newcum <dee.newcum@gmail.com>
2016-10-27 12:25:29 -0500

    add pe9.py

---
+++ project_euler/pe9.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import math
+import sys
+import pprint           # pprint.pprint(["hello", "world"])
+
+for a in range(1, 1001):
+    for b in range(a, 1001):
+        c = math.sqrt(a*a + b*b)
+        if a + b + c == 1000:
+            print a
+            print b
+            print a * b * c
+            sys.exit()
+            
